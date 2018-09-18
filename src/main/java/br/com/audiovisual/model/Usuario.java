a7caee41387c62a14b40b22606dbbce9f67c7423
gustavo-Muller <gustavo.muller.sousa@gmail.com>
2018-09-18 18:03:22 -0300

    Adiciona usuario na tabela(Incompleto)

--- src/main/java/br/com/audiovisual/model/Usuario.java
+++ src/main/java/br/com/audiovisual/model/Usuario.java
@@ -1,53 +1,62 @@
 package br.com.audiovisual.model;
 
 import br.com.audiovisual.enuns.TipoUsuario;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class Usuario {
 
-	private Long id;
-	private String nome;
-	private String sobrenome;
-	private String telefone;
-	private TipoUsuario tipoUsuario;
-
-	public Long getId() {
+	private LongProperty id;
+	private StringProperty nome;
+	private StringProperty email;
+	private StringProperty telefone;
+	private ObjectProperty<TipoUsuario> tipoUsuario;
+	
+	public Usuario(Long i, String nome, String email, String telefone, TipoUsuario tipoUsuario) {
+		this.id = new SimpleLongProperty(i);
+		this.nome = new SimpleStringProperty(nome);
+		this.email = new SimpleStringProperty(email);
+		this.telefone = new SimpleStringProperty(telefone);
+		this.tipoUsuario = new SimpleObjectProperty<TipoUsuario>(tipoUsuario);
+	}
+	
+	public LongProperty getId() {
 		return id;
 	}
-
-	public void setId(Long id) {
+	public void setId(LongProperty id) {
 		this.id = id;
 	}
-
-	public String getNome() {
+	public StringProperty getNome() {
 		return nome;
 	}
-
-	public void setNome(String nome) {
+	public void setNome(StringProperty nome) {
 		this.nome = nome;
 	}
-
-	public String getSobrenome() {
-		return sobrenome;
+	public StringProperty getEmail() {
+		return email;
 	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
+	public void setEmail(StringProperty email) {
+		this.email = email;
 	}
-
-	public String getTelefone() {
+	public StringProperty getTelefone() {
 		return telefone;
 	}
-
-	public void setTelefone(String telefone) {
+	public void setTelefone(StringProperty telefone) {
 		this.telefone = telefone;
 	}
-
-	public TipoUsuario getTipoUsuario() {
+	public ObjectProperty<TipoUsuario> getTipoUsuario() {
 		return tipoUsuario;
 	}
-
-	public void setTipoUsuario(TipoUsuario tipoUsuario) {
+	public void setTipoUsuario(ObjectProperty<TipoUsuario> tipoUsuario) {
 		this.tipoUsuario = tipoUsuario;
 	}
+
+	
+	
+
 
 }
