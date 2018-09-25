e31aad1eb8e944f5907ee45ae30c12618b7be6c0
Gustavo <gusta-of>
2018-09-25 20:09:19 -0300

    Implementando a DAO de usuario

--- src/main/java/br/com/audiovisual/Main.java
+++ src/main/java/br/com/audiovisual/Main.java
@@ -1,26 +1,25 @@
 package br.com.audiovisual;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("view/FXMLInicio.fxml"));
-			Scene scene = new Scene(root,800,600);
+			Scene scene = new Scene(root, 800, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
